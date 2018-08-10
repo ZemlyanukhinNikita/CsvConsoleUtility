564e475679bb0369a2b347481bcb348a86a792b4
n.zemlyanukhin <n.zemlyanukhin@robo.finance>
2018-08-10 14:28:40 +0700

    some refactoring

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-require 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 require_once 'services/CliParserService.php';
-require_once 'domain/Options.php';
 require_once 'services/CsvService.php';
+require_once 'domain/Options.php';
 
 $opt = new CliParserService();
 $options = $opt->parse();
@@ -22,32 +22,27 @@ if ($help) {
 }
 
 if (!$pathToInputFile || !$pathToConfigFile || !$pathToOutputFile) {
-    echo 'Пример ввода: -i input.csv -c config.php -o output.csv (обязательные параметры)';
+    echo 'Пример ввода: -i input.csv -c config.php -o output.csv (обязательные параметры)' . PHP_EOL;
     $opt->printHelp();
     exit(1);
 }
 
-$encoding = mb_detect_encoding(file_get_contents($pathToInputFile),['UTF-8', 'Windows-1251']);
-//$encoding = mb_check_encoding(file_get_contents($pathToInputFile),'UTF-8') ? 'UTF-8' : 'Windows-1251';
+$encoding = mb_detect_encoding(file_get_contents($pathToInputFile), ['UTF-8', 'Windows-1251']);
 if ($encoding !== 'UTF-8' && $encoding !== 'Windows-1251') {
-    echo 'Неверная кодировка исходного файла';
+    echo 'Неверная кодировка исходного файла' . PHP_EOL;
     exit(1);
 }
-
-iconv_set_encoding('output_encoding', $encoding);
 
 $config = require_once $pathToConfigFile;
 
 $csvService = new CsvService();
-
-$inputData = $csvService->readCsv($pathToInputFile,$delimiter);
+$inputData = $csvService->readCsv($pathToInputFile, $delimiter);
 
 if ($strict && !$csvService->isStrict($inputData, $config)) {
-    echo 'Конфигурационный файл имеет больше столбцов чем исходный';
+    echo 'Конфигурационный файл имеет больше столбцов чем исходный' . PHP_EOL;
     exit(1);
 }
 
 $newCsvData = $csvService->generateOutputDataFromConfig($inputData, $config, $skipFirst);
 $csvService->writeCsv($newCsvData, $pathToOutputFile, $delimiter, $encoding);
-echo 'Файл '. $pathToOutputFile . ' успешно создан или обновлен' . PHP_EOL;
-exit(0);
+echo 'Файл ' . $pathToOutputFile . ' успешно создан или обновлен' . PHP_EOL;
